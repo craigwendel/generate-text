b350eb2504e3d91360fd5be9d8f2192c166fa091
Craig Wendel <wendelcraig@gmail.com>
2017-06-19 15:08:22 -0500

    Edit text.js to make the application run once per page load vs. once per application.

--- text.js
+++ text.js
@@ -1,16 +1,15 @@
 const loremIpsum = require('lorem-ipsum')
-let output = loremIpsum({
-  count: 3,
-  units: 'paragraphs',
-  format: 'HTML',
-  suffix: '<br/>'
-})
 const express = require('express')
 const app = express()
 
 app.get('/lorem', function (req, res) {
+  let output = loremIpsum({
+    count: 3,
+    units: 'paragraphs',
+    format: 'HTML',
+    suffix: '<br/>',
+  })
   res.send(output)
-  console.log('This is a paragraph with lorem-ipsum')
 })
 app.listen(3000, function (){
   console.log('You started the text app!')
